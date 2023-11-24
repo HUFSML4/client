cb9e9f6e1159fa42b460e2c802f96097dfbc5cf5
양민석 <kurooru98@gmail.com>
2023-11-24 12:53:55 +0900

    header footer implement

--- src/style/global/header/body.jsx
+++ src/style/global/header/body.jsx
@@ -1,9 +1,9 @@
 import { BodyDiv } from "./style";
 function Body() {
-
-
     return (
         <BodyDiv>
+            <h1>내 인생영화와 비슷한 영화를 추천해 줘!</h1>
+            <p><span>knn 기반</span> 영화 추천 서비스</p>
         </BodyDiv>
     );
 }
--- src/style/global/header/style.jsx
+++ src/style/global/header/style.jsx
@@ -1,5 +1,36 @@
 import styled from "styled-components";
-
+import movie from "../../../assets/images/movie.png"
 export const BodyDiv = styled.div`
 
+    && h1:nth-child(1){
+        margin: 0;
+        font-size: 130%;
+        font-family: sans-serif;
+        font-weight: 500;
+        text-align: center;
+    }
+
+    && p:nth-child(2){
+        margin: 0;
+        font-size: 100%;
+        font-family: sans-serif;
+        font-weight: 500;
+        text-align: center;
+    }
+    && p:nth-child(2)::after {
+        content: "";
+        display: inline-block;
+        width: 16px;
+        height: 16px;
+        background-image: url(${movie});
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+        transform: translateY(2px);
+        margin-left: 1%;
+    }
+    && p:nth-child(2) span{
+        text-decoration: underline;
+        color: navy;
+    }
 `
