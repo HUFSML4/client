434174cda2d434b2437e26f94469ed8881343d62
양민석 <kurooru98@gmail.com>
2023-11-24 13:03:52 +0900

    header update

--- src/style/global/header/style.jsx
+++ src/style/global/header/style.jsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import movie from "../../../assets/images/movie.png"
 export const BodyDiv = styled.div`
 
     && h1:nth-child(1){
@@ -16,18 +15,6 @@ export const BodyDiv = styled.div`
         font-family: sans-serif;
         font-weight: 500;
         text-align: center;
-    }
-    && p:nth-child(2)::after {
-        content: "";
-        display: inline-block;
-        width: 16px;
-        height: 16px;
-        background-image: url(${movie});
-        background-position: center;
-        background-size: cover;
-        background-repeat: no-repeat;
-        transform: translateY(2px);
-        margin-left: 1%;
     }
     && p:nth-child(2) span{
         text-decoration: underline;
