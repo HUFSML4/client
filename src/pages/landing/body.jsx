12fb38cc6fdaa1f20be54c0cd4af6cc31618bf00
양민석 <kurooru98@gmail.com>
2023-11-24 13:33:35 +0900

    landing framing

--- src/pages/landing/body.jsx
+++ src/pages/landing/body.jsx
@@ -3,7 +3,22 @@ function Body() {
 
     return(
         <BodyDiv>
-            
+            <h1>STEP 1.<br />
+                <span>내 인생 영화</span>를 검색해 주세요!
+            </h1>
+            <p>
+                <input type="text" name="" id="" placeholder="ex) spiderman"/><button>검색</button>
+            </p>
+            <h1>STEP 2.<br />
+                검색 결과 중 <span>내 인생 영화</span>를 클릭해 주세요!
+            </h1>
+            <section id="searchResultSection">
+                <p>검색 결과가 없어요 ㅠㅠ</p>
+            </section>
+            <h1>STEP 3.<br />
+                <span>"비슷한 영화 찾기"</span> 버튼을 클릭해 주세요!
+            </h1>
+            <button>비슷한 영화 찾기</button>
         </BodyDiv>
     )
 }
--- src/pages/landing/style.jsx
+++ src/pages/landing/style.jsx
@@ -1,5 +1,91 @@
 import styled from "styled-components";
 
 export const BodyDiv = styled.div`
+    margin: 30px auto;
+    width: 90%;
+
+    && h1:nth-child(1){
+        margin: 0;
+        font-size: 100%;
+        font-weight: 600;
+        line-height: 1.5;
+        span{
+            color: navy;
+        }
+    }
+
+    && p:nth-child(2){
+        margin: 0;
+        margin-bottom: 5%;
+
+        input{
+            width: 85%;
+            padding: 0;
+            font-size: 100%;
+            border: 2px solid navy;
+            box-sizing: border-box;
+            border-radius: 10px;
+            line-height: 2;
+            padding-left: 3%;
+        }
+        button{
+            width: 15%;
+            padding: 0;
+            font-size: 100%;
+            line-height: 2;
+            border-radius: 10px;
+            background-color: #fff;
+        }
+    }
+
+    && h1:nth-child(3){
+        margin: 0;
+        font-size: 100%;
+        font-weight: 600;
+        line-height: 1.5;
+        span{
+            color: navy;
+        }
+    }
+
+    && section:nth-child(4){
+        width: 100%;
+        height: 200px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        border: 2px solid navy;
+        box-sizing: border-box;
+        margin-bottom: 5%;
+        p{
+            margin: 0;
+            text-align: center;
+            font-size: 100%;
+            font-weight: 500;
+        }
+    }
+
+    && h1:nth-child(5){
+        margin: 0;
+        font-size: 100%;
+        font-weight: 600;
+        line-height: 1.5;
+        span{
+            color: navy;
+        }
+    }
+    && button:nth-child(6){
+        background-color: #fff;
+        border: 2px solid navy;
+        border-radius: 10px;
+        box-sizing: border-box;
+        line-height: 2;
+        font-size: 100%;
+        font-family: sans-serif;
+        display: block;
+        margin: 5% auto;
+        width: 50%;
+        font-weight: 600;
+    }
 
 `
