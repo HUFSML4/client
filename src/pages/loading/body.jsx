a7f70df8e7f565e3565261d7af2a5c91e43dfb62
양민석 <kurooru98@gmail.com>
2023-11-24 13:59:28 +0900

    loading framing

--- src/pages/loading/body.jsx
+++ src/pages/loading/body.jsx
@@ -3,7 +3,8 @@ function Body() {
 
     return(
         <BodyDiv>
-            
+            <div></div>
+            <p>추천 영화를 찾는 중입니다...</p>
         </BodyDiv>
     )
 }
--- src/pages/loading/style.jsx
+++ src/pages/loading/style.jsx
@@ -1,5 +1,21 @@
 import styled from "styled-components";
-
+import search from "../../assets/images/search.gif";
 export const BodyDiv = styled.div`
+    margin: 30px 0;
+    && div:nth-child(1){
+        width: 120px;
+        height: 120px;
+        margin: 0 auto;
+        background-image: url(${search});
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: contain;
+    }
 
+    && p:nth-child(2){
+        margin: 0;
+        text-align: center;
+        line-height: 2;
+        font-size: 90%;
+    }
 `
