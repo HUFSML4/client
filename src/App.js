7740d84fc3972e229d69ccd76464793a155b8a58
양민석 <kurooru98@gmail.com>
2023-11-24 12:19:51 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Landing from "./pages/landing";
+import Loading from "./pages/loading";
+import Result from "./pages/result";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path={`/`} element={<Landing />}></Route>
+        <Route path={`/loading`} element={<Loading />}></Route>
+        <Route path={`/result`} element={<Result />}></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/style/global/footer/body.jsx
@@ -0,0 +1,11 @@
+import { BodyDiv } from "./style";
+function Body() {
+
+    return (
+        <BodyDiv>
+            <p>ⓒ 2023. HUFS_ML_4 all rights reserved.</p>
+        </BodyDiv>
+    );
+}
+
+export default Body;
