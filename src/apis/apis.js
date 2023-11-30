8a90a6b57ec9f5e3536601cc12b4686197e5ac06
양민석 <kurooru98@gmail.com>
2023-11-30 14:26:54 +0900

    edit server url (http -> https)

--- src/apis/apis.js
+++ src/apis/apis.js
@@ -8,14 +8,14 @@ const serverApi = axios.create({
 
 export const searchMovies = async (keyword) => {
     let currMovies;
-    await serverApi.post(`http://3.36.128.245:8000/searching/`, {"text":keyword}).then((response) => {
+    await serverApi.post(`https://3.36.128.245:8000/searching/`, {"text":keyword}).then((response) => {
         currMovies = response.data.data;
     })
     return currMovies;
 }
 
 export const getRecommendMovies = async (navigate, movieTitle) => {
-    await serverApi.get(`http://3.36.128.245:8000/searching/recommend/${movieTitle}/`).then((response) => {
+    await serverApi.get(`https://3.36.128.245:8000/searching/recommend/${movieTitle}/`).then((response) => {
         const recommendMovies = response.data.data;
         navigate(`/result`, {state: {"recommendMovies": recommendMovies}});
     })
