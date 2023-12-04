618ca59026a83014368a9727c2586237e4c17f55
양민석 <kurooru98@gmail.com>
2023-12-04 09:05:03 +0900

    update api url

--- src/apis/apis.js
+++ src/apis/apis.js
@@ -8,14 +8,14 @@ const serverApi = axios.create({
 
 export const searchMovies = async (keyword) => {
     let currMovies;
-    await serverApi.post(`https://port-0-hufsml-1gksli2alpkrtsy0.sel4.cloudtype.app/searching/`, {"text":keyword}).then((response) => {
+    await serverApi.post(`https://port-0-hufsml-1gksli2alpnlcd7w.sel4.cloudtype.app/searching/`, {"text":keyword}).then((response) => {
         currMovies = response.data.data;
     })
     return currMovies;
 }
 
 export const getRecommendMovies = async (navigate, movieTitle) => {
-    await serverApi.get(`https://port-0-hufsml-1gksli2alpkrtsy0.sel4.cloudtype.app/searching/recommend/${movieTitle}/`).then((response) => {
+    await serverApi.get(`https://port-0-hufsml-1gksli2alpnlcd7w.sel4.cloudtype.app/searching/recommend/${movieTitle}/`).then((response) => {
         const recommendMovies = response.data.data;
         navigate(`/result`, {state: {"recommendMovies": recommendMovies}});
     })
